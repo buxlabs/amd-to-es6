6a0c93cf3c54b1aaae50b83b35e7221655511cdc
Emil Ajdyna <emil@ajdyna.com>
2016-09-26 17:36:38 +0200

    add: basic cli

---
+++ bin/cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const fs = require("fs");
+const path = require("path");
+const amdtoes6 = require("../index");
+
+program.parse(process.argv);
+
+var file = program.args[0];
+var content = fs.readFileSync(path.join(__dirname, file), "utf8");
+var compiled = amdtoes6(content);
+
+process.stdout.write(compiled);
