71f09507ce39305af10f464d4b80271a27baf6c4
Emil Ajdyna <emil@ajdyna.com>
2016-09-27 16:33:30 +0200

    add: failing test case

---
+++ test/fixture/app/controller_2/output.js
@@ -0,0 +1,10 @@
+import Marionette from 'backbone.marionette';
+import sideEffect from 'side-effect';
+
+export default Marionette.Object.extend({
+
+    initialize: function (options) {
+        this.window.hello();
+    }
+
+});
--- test/spec/index.js
+++ test/spec/index.js
@@ -15,104 +15,105 @@ function test (dir) {
     const input = fs.readFileSync(file1, "utf8");
     const output = fs.readFileSync(file2, "utf8");
     const result = converter(input);
-    if (dir === "app/behavior_1") {
-        console.log(output);
-        console.log(result);
-    }
     return compare(result, output);
 }
 
-tap.test("the converter should be available", function (t) {
+tap.test("the converter should be available", (t) => {
     t.assert(typeof converter === "function");
     t.end();
 });
 
-tap.test("it should convert empty array in define correctly", function (t) {
+tap.test("it should convert empty array in define correctly", (t) => {
     t.assert(test("define-callback-empty-array"));
     t.end();
 });
 
-tap.test("it should convert empty array in define with array function correctly", function (t) {
+tap.test("it should convert empty array in define with array function correctly", (t) => {
     t.assert(test("define-arrow-function-callback-empty-array"));
     t.end();
 });
 
-tap.test("it should convert define with callback only correctly using the built in parser", function (t) {
+tap.test("it should convert define with callback only correctly using the built in parser", (t) => {
     t.assert(test("define-callback-only"));
     t.end();
 });
 
-tap.test("it should convert empty define with arrow function correctly", function (t) {
+tap.test("it should convert empty define with arrow function correctly", (t) => {
     t.assert(test("define-arrow-function-callback-only"));
     t.end();
 });
 
-tap.test("it should convert define with deps correctly", function (t) {
+tap.test("it should convert define with deps correctly", (t) => {
     t.assert(test("define-arrow-function-callback-with-deps"));
     t.end();
 });
 
-tap.test("it should convert define with deps with arrow function correctly", function (t) {
+tap.test("it should convert define with deps with arrow function correctly", (t) => {
     t.assert(test("define-callback-with-deps"));
     t.end();
 });
 
-tap.test("it should convert define with an object in callback only correctly", function (t) {
+tap.test("it should convert define with an object in callback only correctly", (t) => {
     t.assert(test("define-object-only"));
     t.end();
 });
 
-tap.test("it should keep dependencies with side effects", { todo: true }, function (t) {
+tap.test("it should keep dependencies with side effects", { todo: true }, (t) => {
     t.assert(test("app/behavior_1"));
     t.end();
 });
 
-tap.test("it should convert controllers correctly", function (t) {
+tap.test("it should convert controllers correctly", (t) => {
     t.assert(test("app/controller_1"));
     t.end();
 });
 
-tap.test("it should leave empty var statements", function (t) {
+tap.test("it should keep dependencies with side effects", { todo: true }, (t) => {
+    t.assert(test("app/controller_2"));
+    t.end();
+});
+
+tap.test("it should leave empty var statements", (t) => {
     t.assert(test("app/helper_1"));
     t.end();
 });
 
-tap.test("it should leave empty var statements", function (t) {
+tap.test("it should leave empty var statements", (t) => {
     t.assert(test("app/helper_2"));
     t.end();
 });
 
-tap.test("it should leave empty var statements", function (t) {
+tap.test("it should leave empty var statements", (t) => {
     t.assert(test("app/model_1"));
     t.end();
 });
 
-tap.test("it should convert views correctly", function (t) {
+tap.test("it should convert views correctly", (t) => {
     t.assert(test("app/view_1"));
     t.end();
 });
 
-tap.test("it should convert modules with functions after the return correctly", function (t) {
+tap.test("it should convert modules with functions after the return correctly", (t) => {
     t.assert(test("app/view_2"));
     t.end();
 });
 
-tap.test("it should convert modules with constructors assigned to variables correctly", function (t) {
+tap.test("it should convert modules with constructors assigned to variables correctly", (t) => {
     t.assert(test("app/view_3"));
     t.end();
 });
 
-tap.test("it should convert subapps correctly", function (t) {
+tap.test("it should convert subapps correctly", (t) => {
     t.assert(test("app/subapp_1"));
     t.end();
 });
 
-tap.test("it should convert modules with one require sugar call expression correctly", function (t) {
+tap.test("it should convert modules with one require sugar call expression correctly", (t) => {
     t.assert(test("app/module_1"));
     t.end();
 });
 
-tap.test("it should convert modules with multiple require sugar call expressions correctly", function (t) {
+tap.test("it should convert modules with multiple require sugar call expressions correctly", (t) => {
     t.assert(test("app/module_2"));
     t.end();
 });
