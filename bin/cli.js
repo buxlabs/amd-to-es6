af257cf90e459f36001bea80865a631d0898eccb
tushark39 <tushark39@gmail.com>
2020-11-02 02:13:20 +0530

    contributed in cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-const fs = require('fs')
-const path = require('path')
-const glob = require('glob')
-const mkdirp = require('mkdirp')
-const amdtoes6 = require('../index')
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const mkdirp = require('mkdirp');
+const amdtoes6 = require('../index');
 
 program
   .option('-s, --src <dirname>', 'Directory of the source files')
@@ -16,7 +16,7 @@ program
   .option('--replace', 'Replace the input files with results')
   .option('--suffix <string>', 'Replace suffix of the files')
   .option('--quotes <type>', 'Single, double or auto quotes in the output', /^(single|double|auto)$/i, 'single')
-  .parse(process.argv)
+  .parse(process.argv);
 
 function replaceSuffix (filename, suffix) {
   return suffix ? filename.replace(/\.js$/, '.' + suffix) : filename
@@ -24,55 +24,55 @@ function replaceSuffix (filename, suffix) {
 
 function getGlob (options) {
   if (options.recursive) {
-    return '**/*.js'
+    return '**/*.js';
   }
-  return program.glob || '*.js'
+  return program.glob || '*.js';
 }
 
 function convertFile (file, options) {
-  var filepath = path.join(process.cwd(), file)
-  var content = fs.readFileSync(filepath, 'utf8')
-  var compiled = amdtoes6(content, options)
+  var filepath = path.join(process.cwd(), file);
+  var content = fs.readFileSync(filepath, 'utf8');
+  var compiled = amdtoes6(content, options);
   if (options.replace) {
-    var destpath = replaceSuffix(filepath, options.suffix)
+    var destpath = replaceSuffix(filepath, options.suffix);
     if (options.suffix) {
-      fs.unlinkSync(filepath)
+      fs.unlinkSync(filepath);
     }
-    fs.writeFileSync(destpath, compiled)
+    fs.writeFileSync(destpath, compiled);
   } else {
-    process.stdout.write(compiled)
+    process.stdout.write(compiled);
   }
 }
 
 function convertFiles (files, options) {
   files.forEach(function (file) {
-    var filepath = path.join(options.src, file)
-    var content = fs.readFileSync(filepath, 'utf8')
-    var compiled = amdtoes6(content, options)
-    var destpath = options.replace ? filepath : path.join(options.dest, file)
+    var filepath = path.join(options.src, file);
+    var content = fs.readFileSync(filepath, 'utf8');
+    var compiled = amdtoes6(content, options);
+    var destpath = options.replace ? filepath : path.join(options.dest, file);
     if (options.suffix) {
-      fs.unlinkSync(filepath)
+      fs.unlinkSync(filepath);
     }
-    destpath = replaceSuffix(destpath, options.suffix)
-    fs.writeFileSync(destpath, compiled)
-  })
+    destpath = replaceSuffix(destpath, options.suffix);
+    fs.writeFileSync(destpath, compiled);
+  });
 }
 
 if (!program.src) {
-  var file = program.args[0]
-  convertFile(file, program)
-  process.exit(0)
+  var file = program.args[0];
+  convertFile(file, program);
+  process.exit(0);
 }
 
 if (program.dest) {
-  mkdirp.sync(program.dest)
+  mkdirp.sync(program.dest);
 }
 
 if (program.src && (program.dest || program.replace)) {
-  var files = glob.sync(getGlob(program), { cwd: program.src })
-  convertFiles(files, program)
-  process.exit(0)
+  var files = glob.sync(getGlob(program), { cwd: program.src });
+  convertFiles(files, program);
+  process.exit(0);
 }
 
-process.stderr.write('Please provide --src and --dest')
-process.exit(1)
+process.stderr.write('Please provide --src and --dest');
+process.exit(1);
